f03f6fec59f79456f92c21d557abc12355d4e868
Fedor Chervyakov <fedor.chervyakov@emlid.com>
2020-05-15 03:17:20 +0300

    vga_controller: add row and column counters, and min_counter_width func

--- src/vga_controller.vhd
+++ src/vga_controller.vhd
@@ -3,6 +3,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
+use IEEE.math_real.all;
 
 entity vga_controller is
     generic (
@@ -53,33 +54,61 @@ architecture A of vga_controller is
         );
     end component;
 
+    -- calculate min counter width
+    function min_counter_width(N : integer)
+    return integer is
+        variable exponent : real;
+    begin
+        exponent := ceil(log2(real(N)));
+        return integer(exponent);
+    end min_counter_width;
+
     -- constants
     constant N_col : -- total number of columns in a row in a frame
         integer := HORIZONTAL+H_FRONT_PORCH+H_SYNC_PULSE+H_BACK_PORCH;
     constant N_row : -- total number of rows in a frame
         integer := VERTICAL+V_FRONT_PORCH+V_SYNC_PULSE+V_BACK_PORCH;
+    constant col_counter_width : -- width of column counter
+        integer := min_counter_width(N_col);
+    constant row_counter_width : -- width of row counter
+        integer := min_counter_width(N_row);
 
     -- counter signals
-    signal Q_col : std_logic_vector(9 downto 0) := (others => '0'); -- column
-    signal Q_row : std_logic_vector(9 downto 0) := (others => '0'); -- row 
-    
+    signal Q_col : std_logic_vector(col_counter_width-1 downto 0)
+        := (others => '0'); -- column
+    signal Q_row : std_logic_vector(row_counter_width-1 downto 0)
+        := (others => '0'); -- row
+
+    -- counter resets;
+    signal col_reset : std_logic;
+    signal row_reset : std_logic;
+
     signal row_clock : std_logic; -- row clock
 begin
     -- column counter
     col_counter : nru_counter
-        generic map (N => 10) 
-        port map (nreset => nreset, clk => clk, Q => Q_col );
+        generic map (N => col_counter_width)
+        port map (nreset => col_reset, clk => clk, Q => Q_col);
 
     -- row counter
     row_counter : nru_counter
-        generic map (N => 10) 
-        port map (nreset => nreset, clk => row_clock, Q => Q_row);
+        generic map (N => row_counter_width)
+        port map (nreset => row_reset, clk => row_clock, Q => Q_row);
 
     -- TODO: video memory
 
+
+    -- column counter reset
+    col_reset <= '0' when Q_col >= N_col-1 or nreset='0' else
+                 '1' ;
+
+    -- row counter reset
+    row_reset <= '0' when Q_row >= N_row-1 or nreset='0' else
+                 '1' ;
+
     -- row clock generation process
     generate_row_clock : process(clk, Q_col)
-        variable res : std_logic := '0';
+        variable res : std_logic := '1';
     begin
         -- the process
         if (rising_edge(clk)) then
