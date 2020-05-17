48470fa24b4b68fd01d874a3e5129dac09ba6e65
Fedor Chervyakov <fedor.chervyakov@emlid.com>
2020-05-17 05:33:02 +0300

    video_memory: add fake video memory with striped pattern

--- src/video_memory.vhd
+++ src/video_memory.vhd
@@ -32,14 +32,8 @@ entity video_memory is
 end entity;
 
 architecture A of video_memory is
-    subtype pixel_t is std_logic_vector(COLOR_BITNESS*3-1 downto 0);
-    type row_t is array(HORIZONTAL-1 downto 0) of pixel_t;
-    type pixel_LUT_t is array(VERTICAL-1 downto 0) of row_t;
-
-
-    -- generate simple striped pattern
-    function pattern (row_addr : integer; col_addr : integer)
-    return pixel_t is
+begin
+    process (col_addr, row_addr)
         variable r, g, b: std_logic_vector(COLOR_BITNESS-1 downto 0);
         variable w : integer;
     begin
@@ -57,55 +51,9 @@ architecture A of video_memory is
             g := (others => '0');
             b := (others => '1');
         end if;
-        return r & g & b;
-    end pattern;
-    
-    -- Create look up table
-    function init_pixel_LUT
-    return pixel_LUT_t is
-        variable tmp_table : pixel_LUT_t;
-        variable tmp_row   : row_t;
-    begin
-        for row in 0 to VERTICAL-1 loop
-            tmp_row := (others => (others => '0'));
-            for col in 0 to HORIZONTAL-1 loop
-               tmp_row(col) := pattern(row, col); 
-            end loop;
-            tmp_table(row) := tmp_row;
-        end loop;
-        return tmp_table;
-    end init_pixel_LUT;
 
-	-- This tells Quartus to create .mif file
-	-- Single Uncompressed image with Memory initialization
-	-- initialization mode has to be selected
-	-- compressed and dual images might work too
-    signal pixel_LUT : pixel_LUT_t := init_pixel_LUT;
-begin
-    process (col_addr, row_addr)
-        variable pixel : pixel_t;
-        variable temp_row : row_t;
-        variable w : integer;
-    begin
-        w := integer(floor(real(HORIZONTAL)/3.0));
-        if (col_addr < w) then
-            r := (others => '1');
-            g := (others => '0');
-            b := (others => '0');
-        elsif (col_addr < w*2) then
-            r := (others => '0');
-            g := (others => '1');
-            b := (others => '0');
-        elsif (col_addr < HORIZONTAL-1) then
-            r := (others => '0');
-            g := (others => '0');
-            b := (others => '1');
-        end if;
-        temp_row := pixel_LUT(conv_integer(row_addr));
-        pixel := temp_row(conv_integer(col_addr));
-
-        RED <= pixel(COLOR_BITNESS*3-1 downto COLOR_BITNESS*2-1);
-        GREEN <= pixel(COLOR_BITNESS*2-1 downto COLOR_BITNESS-1);
-        BLUE <= pixel(COLOR_BITNESS-1 downto 0);
+        RED <= r;
+        GREEN <= g;
+        BLUE <= b;
     end process;
 end architecture A;
